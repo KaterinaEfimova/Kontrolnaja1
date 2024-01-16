d5b3b342290990f080aa8572d136bf88df283d94
«KaterinaEfimova» <efimovaen2016@mail.ru>
2024-01-16 13:09:48 +0300

    убрала ошибку CS8601

--- Массив/Program.cs
+++ Массив/Program.cs
@@ -8,7 +8,7 @@ void array(string [] stringArray)
     for (int i = 0; i < stringArray.Length; i++)
     {
         Console.WriteLine($"Введите {i + 1} элемент массива");
-        stringArray[i] = Console.ReadLine();
+        stringArray[i] = Console.ReadLine()!;
     }
 }
 
